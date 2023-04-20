92a5cbd7ece7f3864f040afc80d4d6d8d84cd5f2
“vinay <vinaypamu18263@gmail.com>
2023-04-20 11:05:16 +0530

    adding stack and linked list

---
+++ 18P61A0263/C_advance/14_linked_list-14-4-23/2_insert_pf.c
@@ -0,0 +1,199 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+typedef struct vv
+{
+	char vv1[10];
+	struct vv *c;
+}vin;
+
+vin * vvcreat(vin *p);
+void list(vin *p);
+void delete(vin **t);
+vin * after(vin *c,vin *p);
+vin * before(vin *c,vin * p);
+vin * insert(vin *t,char b);
+vin * atposition(vin *p);
+
+int main()
+{
+	vin *p=NULL,*n=NULL,*l=NULL;
+	char a=0;
+	while(1)
+	{
+		n=vvcreat(p);
+		if(p==NULL)
+			p=n;
+		else 
+			l->c=n;
+		l=n;
+		printf("enter y/n for  continue/discontinue \n");
+		scanf(" %c",&a);
+		if(a=='n')
+			break;
+	}
+	n=NULL;
+	while(1)
+	{
+		printf("==>  enter   <==\n  i for insert\n  a for after insert\n  b for before insert\n  d for delete\n  n for discontinue \n  p for print\n");
+		scanf(" %c",&a);
+		if(a=='i')
+			n=atposition(p);
+		else if(a=='p')
+			list(p);
+		else if(a=='a')
+			n=insert(p,a);
+		else if(a=='b')
+			n=insert(p,a);
+		else if(a=='d')
+			delete(&p);
+		else if(a=='n')
+			break;
+		if(n!=NULL&&a!='p'&&a!='d')
+				p=n;
+	}
+	list(p);
+	return 0;
+}
+
+void input(vin *p)
+{
+	printf("enter string \n");
+		scanf(" %9[^\n]s",p->vv1);
+}
+
+vin * vvcreat(vin *p)
+{
+	p=(vin *) malloc(sizeof(vin));
+	if(p==NULL)
+		return NULL;
+	p->c=NULL;
+	input(p);
+	return p;
+}
+
+void list(vin *p)
+{
+	while(p)
+	{
+		printf("%s\n",p->vv1);
+		p=p->c;
+	}
+}
+
+vin * after(vin *c,vin *p)
+{
+	for(int j=1;;j++)
+	{
+		if(strcmp(p->vv1,c->vv1)==0)
+			return p;
+		p=p->c;
+		if(p==NULL)
+		{
+			printf("maximum node comp is reached match not found\n");
+			return NULL;
+		}
+	}
+}
+
+vin * before(vin *c,vin * p)
+{
+	for(int j=1;;j++)
+	{
+		if((strcmp(p->vv1,c->vv1)==0)&&(j==1))
+			return p;
+		else if(strcmp(p->c->vv1,c->vv1)==0)
+			return p;
+		p=p->c;
+		if(p->c==NULL)
+		{
+			printf("maximum node comp is reached match not found\n");
+			break;
+		}
+	}
+	return NULL;
+}
+
+vin * insert(vin *t,char b)
+{
+	vin *w=NULL,*m=t;
+	w=vvcreat(t);
+	if(b=='b')
+	{
+		t=before(w,m);
+loop:
+		if(t!=NULL)
+		input(w);
+	}
+	else if(b=='a')
+	{
+		t=after(w,m);
+		goto loop;
+	}
+	if(t!=NULL&&((b=='a')||(t!=m)||(b=='i')))//return values of function
+	{
+		w->c=t->c;
+		t->c=w;
+	}
+	else if(t==m&&b!='a')
+	{
+	w->c=t;
+	return w;
+	}
+	return NULL;
+}
+
+vin * atposition(vin *p)
+{
+	int i;
+	vin *w=NULL;
+	printf("enter insert position\n");
+	scanf("%d",&i);
+	if(p==NULL&&i!=1)
+		return NULL;
+	if(i==1)
+	{
+		w=vvcreat(p);
+		w->c=p;
+		return w;
+	}
+	for(int j=1;j<i-1;j++)
+	{
+		if(p->c==NULL)
+			return NULL;
+		p=p->c;
+	}
+	if(i>0)
+	insert(p,'i');
+	return NULL;
+}
+
+void delete(vin **t)
+{
+	int i=0,j=0;
+	if((*t)==NULL)
+		return;
+	vin *t1=(*t)->c,*t2=*t;;
+	printf("enter position to delete\n");
+	scanf("%d",&i);
+	for(j=1;j<i-1;j++)
+	{
+		if(t1==NULL)
+		{
+			printf("given position is out of boundary\n");
+			return ;
+		}
+		t2=t1;
+		t1=t1->c;
+	}
+	if(i==1)
+	{
+		*t=(*t)->c;free(t2);
+	}
+	else if((*t)->c!=NULL)
+	{
+		t2->c=t1->c;free(t1);
+	}
+}
+
---
+++ 18P61A0263/C_advance/14_linked_list-14-4-23/README.md
@@ -0,0 +1,21 @@
+1) Create a sorted linked list from the numbers entered by the user by finding the appropriate place to insert them, and thereby keep the linked list always in a sorted state.
+
+
+Example:
+user input: 3
+
+Linked List: 3
+
+user next input : 5
+Linked List : 3 5
+
+user next input : 4
+Linked List: 3 4 5 ( insert 4 after 3, before 5)
+
+user next input : 7
+
+Linked List : 3 4 5 7
+
+user next input : 1
+Linked List : 1 3 4 5 7 
+
